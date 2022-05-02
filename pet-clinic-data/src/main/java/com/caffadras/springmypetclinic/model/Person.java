a4984effaef4d39e5e62dc9d8fde0429e244a1a7
Caffadras <senya.dubik@gmail.com>
2022-05-02 18:54:40 +0300

    Adding Base Entity. Closes #4

--- pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package com.caffadras.springmypetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package com.caffadras.springmypetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
     private String name;
 
     public String getName() {
