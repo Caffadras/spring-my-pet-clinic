aba7e850c0b48da849c15b5e7bff9b5e2c91c0a4
Caffadras <senya.dubik@gmail.com>
2022-05-02 15:57:55 +0300

    adding POJOs. Closes #2.

---
+++ src/main/java/com/caffadras/springmypetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.caffadras.springmypetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/caffadras/springmypetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.caffadras.springmypetclinic.model;
+
+public class Vet extends Person{
+}
