9fe6414a2f33a0d5ab56c62e87436398d7775261
Caffadras <senya.dubik@gmail.com>
2022-05-07 17:46:52 +0300

    Refactoring Service interfaces to common base interface. Closes #8

--- pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package com.caffadras.springmypetclinic.services;
 
 import com.caffadras.springmypetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/caffadras/springmypetclinic/services/VetService.java
@@ -2,12 +2,6 @@ package com.caffadras.springmypetclinic.services;
 
 import com.caffadras.springmypetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet owner);
-
-    Set<Vet> findAll();
 }
